6428ef3c0f013d1ba77c0d28e688e62e24ec383c
vexpi <lolodelo67@gmail.com>
2025-04-25 23:11:18 +0200

    Update

--- src/main/kotlin/fr/brighterbuilds/Ikea.kt
+++ src/main/kotlin/fr/brighterbuilds/Ikea.kt
@@ -4,12 +4,13 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object Ikea : ModInitializer {
+	const val MOD_ID = "ikea"
     private val logger = LoggerFactory.getLogger("ikea")
 
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+		logger.info("Mod chargé !")
 	}
 }
