3b821b4ca3cc6f83e805a42167e2b54869206fde
vexpi <lolodelo67@gmail.com>
2025-04-25 22:25:44 +0200

    Initial Commit

---
+++ src/client/kotlin/fr/brighterbuilds/IkeaDataGenerator.kt
@@ -0,0 +1,9 @@
+package fr.brighterbuilds
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object IkeaDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
